663a10a20bf755e4e125d3abca88356d85fd41ef
Md Iqbal Hossain <iqbal.hossain9@gmail.com>
2022-04-20 13:06:34 +0600

    Order functionality completed

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace Tangy_DataAccess.Data
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Product>? Products { get; set; }
         public DbSet<ProductPrice>? ProductPrices { get; set; }
+        public DbSet<OrderHeader>? OrderHeaders { get; set; }
+        public DbSet<OrderDetail>? OrderDetails { get; set; }
     }
 }
 
