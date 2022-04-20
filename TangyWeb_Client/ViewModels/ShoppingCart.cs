c52badc8abfd572c5c8fb04e372c2bd112bf7da5
Md Iqbal Hossain <iqbal.hossain9@gmail.com>
2022-04-20 10:29:56 +0600

    Completed shopping cart functionality

---
+++ TangyWeb_Client/ViewModels/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using Tangy_Models;
+
+namespace TangyWeb_Client.ViewModels
+{
+    public class ShoppingCart
+    {
+        public int ProductId { get; set; }
+        public ProductDTO ProductDTO { get; set; }
+        public int ProductPriceId { get; set; }
+        public ProductPriceDTO ProductPriceDTO { get; set; }
+        public int Count { get; set; }
+    }
+}
