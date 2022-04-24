1c8360953add28ceb79c45dc9badea9b93cb9485
Md Iqbal Hossain <iqbal.hossain9@gmail.com>
2022-04-24 12:02:07 +0600

    Stripe payment done

--- Tangy_Common/SD.cs
+++ Tangy_Common/SD.cs
@@ -19,7 +19,8 @@ namespace Tangy_Common
         public const string Role_Admin = "Admin"; 
         public const string Role_Customer = "Customer";
 
-        public const string Local_Token = "JWT Token"; 
-        public const string Local_UserDetails = "UserDetails"; 
+        public const string Local_Token = "JWT Token";
+        public const string Local_OrderDetails = "Local_OrderDetails";
+        public const string Local_UserDetails = "UserDetails";
     }
 }
